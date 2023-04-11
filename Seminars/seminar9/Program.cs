e9e1fad9be144b7218df842040a5530afe0ed641
julia <ulla_89@mail.ru>
2023-04-11 22:15:05 +0300

    семинар задачи 9

--- Seminars/seminar9/Program.cs
+++ Seminars/seminar9/Program.cs
@@ -31,3 +31,46 @@
 
 // Console.Write(Findsum(45));
 
+//Задача 3. Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+
+//M = 1; N = 5. -> "1, 2, 3, 4, 5"
+//M = 4; N = 8. -> "4, 5, 6, 7, 8"
+
+// void NaturNumbers(int m, int n)
+// {
+//     if (n != m)
+//     {
+//         if (n > m)
+//         {
+//             NaturNumbers(m, n - 1);
+//             Console.Write(n + " ");
+//         }
+//         else
+//         {
+//             Console.Write(n + " ");
+//             NaturNumbers(m, n + 1);
+//         }
+
+//     }
+//     else
+//         Console.Write(n + " ");
+// }
+// NaturNumbers(8,1);
+
+//Задача 4.Напишите программу, которая на вход принимает два числа A и B, и возводит число А в целую степень B с помощью рекурсии.
+
+//A = 3; B = 5 -> 243 (3⁵)
+//A = 2; B = 3 -> 8
+
+
+// int FindStepen(int a, int b)
+// {
+// if (b > 1)
+// {
+// return FindStepen(a, b-1) * a;
+// }
+// else
+// return a;
+// }
+
+// Console.WriteLine(FindStepen(2, 3));
